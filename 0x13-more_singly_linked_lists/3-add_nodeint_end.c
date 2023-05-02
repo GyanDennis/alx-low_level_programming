e11a821b667b6e1c13ffe0d6013f99fd736722e7
GyanDennis <gyangh23@gmail.com>
2023-05-02 03:42:13 -0700

    Task 3

--- 0x13-more_singly_linked_lists/3-add_nodeint_end.c
+++ 0x13-more_singly_linked_lists/3-add_nodeint_end.c
@@ -1,32 +1,34 @@
-#include "lists.h" 
-  
- /** 
-  * add_nodeint_end - adds a node to the end of a linked list 
-  * @head: pointer to the head of the list 
-  * @n: number to be used as content 
-  * 
-  * Return: address of the newly added node 
-  */ 
- listint_t *add_nodeint_end(listint_t **head, const int n) 
- { 
-         listint_t *new_node; 
-         listint_t *cursor = *head; 
-  
-         new_node = malloc(sizeof(listint_t)); 
-         if (new_node != NULL) 
-         { 
-                 new_node->n = n; 
-                 new_node->next = NULL; 
-         } 
-         else 
-                 return (NULL); 
-         if (cursor != NULL) 
-         { 
-                 while (cursor->next != NULL) 
-                         cursor = cursor->next; 
-                 cursor->next = new_node; 
-         } 
-         else 
-                 *head = new_node; 
-         return (new_node); 
- }
+#include "lists.h"
+#include <stdlib.h>
+/**
+  * add_nodeint_end - add node at end of a listint_t list.
+  *
+  * @head: head of double pointer
+  * @n: int add the list
+  * Return: NULL if it failed
+  */
+listint_t *add_nodeint_end(listint_t **head, const int n)
+{
+	listint_t *new;
+	listint_t *temp;
+
+	if (head == NULL)
+		return (NULL);
+	new = malloc(sizeof(listint_t));
+	if (new == NULL)
+		return (NULL);
+	new->n = n;
+	new->next = NULL;
+	if (*head == NULL)
+	{
+	*head = new;
+		return (new);
+	}
+	temp = *head;
+	while (temp->next != NULL)
+	{
+		temp = temp->next;
+	}
+	temp->next = new;
+	return (new);
+}
