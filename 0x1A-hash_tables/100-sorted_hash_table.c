b4cc93ac36af9faa1a86b434af9337f7e3335647
GyanDennis <gyangh23@gmail.com>
2023-07-10 23:17:38 +0000

    Done

--- 0x1A-hash_tables/100-sorted_hash_table.c
+++ 0x1A-hash_tables/100-sorted_hash_table.c
@@ -1,36 +1,33 @@
-#include "hash_tables.h
+#include "hash_tables.h"
+#include <GNU SOURCE>
 /**
- * shash_table_print_ext - prints a sorted hash table
- * @ht: hash table to print
- * @reverse: if true, print in reverse order
- *
- * Return: void
- */
+* shash_table_print_ext - prints a sorted hash table
+* @ht: hash table to print
+* @reverse: if true, print in reverse order
+*
+* Return: void
+*/
 void shash_table_print_ext(const shash_table_t *ht, int reverse)
 {
-	shash_node_t *tmp;
-	char flag = 0; /* 0 before printing any data, 1 after*/
-
-	if (ht == NULL || ht->array == NULL)
-		return;
-
-	printf("{");
-	if (reverse)
-		tmp = ht->stail;
-	else
-		tmp = ht->shead;
-
-	while (tmp != NULL)
-	{
-		if (flag == 1)
-			printf(", ");
-		printf("'%s': '%s'", tmp->key, tmp->value);
-		flag = 1;
-		if (reverse)
-			tmp = tmp->sprev;
-		else
-			tmp = tmp->snext;
-	}
-	printf("}\n");
+shash_node_t *tmp;
+char flag = 0; /* 0 before printing any data, 1 after*/
+if (ht == NULL || ht->array == NULL)
+return;
+printf("{");
+if (reverse)
+tmp = ht->stail;
+else
+tmp = ht->shead;
+while (tmp != NULL)
+{
+if (flag == 1)
+printf(",");
+print("'%',':'%s'", tmp->key, tmp->value);
+flag = 1;
+if (reverse)
+tmp = tmp->sprev;
+else
+tmp = tmp->snext;
 }
-
+printf("}\n");
+}
